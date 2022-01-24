3f9490fbefea89711fd4dd1f3a165fe4617e1dce
Qwarkeh <christine.f.stark@gmail.com>
2022-01-24 13:48:31 -0500

    added integer RNG for values between 1 and 3

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,1 +1,9 @@
+function computerPlay () {
+    //generate a random integer between 1 and 3
+    randomNumber = Math.random();
+    scaledNumber = Math.floor((Math.random() * 3) + 1);
+
+    //return "rock", "paper", or "scissors"
+
+}
 
