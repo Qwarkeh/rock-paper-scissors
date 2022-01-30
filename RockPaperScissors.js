1df8c09285e512c64154eb9f5f51d88e18060506
Qwarkeh <christine.f.stark@gmail.com>
2022-01-30 12:33:36 -0500

    win counts and overall winner dynamically displayed

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,19 +1,18 @@
 let playerWins = 0;
 let computerWins = 0;
+let ties = 0;
 let roundCnt = 1;
 
 function game () {
     
-
-
     //Event handlers
     const btnRock = document.querySelector('#btnRock');
     btnRock.addEventListener('click', () => {
+        removeScore();
         console.log(`ROUND ${roundCnt}`);
         let computerSelection = computerPlay();
         playRound(computerSelection, 'Rock');
-        //roundCnt += 1;
-        winChecker(roundCnt)
+        winChecker(roundCnt);
     });
 
     const btnPaper = document.querySelector('#btnPaper');
@@ -21,8 +20,7 @@ function game () {
         console.log(`ROUND ${roundCnt}`);
         let computerSelection = computerPlay();
         playRound(computerSelection, 'Paper');
-        //roundCnt += 1;
-        winChecker(roundCnt)
+        winChecker(roundCnt);
     });
 
     const btnScissors = document.querySelector('#btnScissors');
@@ -30,10 +28,8 @@ function game () {
         console.log(`ROUND ${roundCnt}`);
         let computerSelection = computerPlay();
         playRound(computerSelection, 'Scissors');
-        //roundCnt += 1;
-        winChecker(roundCnt)
+        winChecker(roundCnt);
     });
-
 }
 
 //user RNG to determine computer's choice 
@@ -55,6 +51,7 @@ function computerPlay () {
 function playRound (computerSelection, playerSelection) {
     if (playerSelection === computerSelection) {
         console.log("Tie!")
+        ties += 1;
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
         console.log("You win, Rock beats Scissors!");
         playerWins += 1;
@@ -78,6 +75,7 @@ function playRound (computerSelection, playerSelection) {
 };
 
 function winChecker () {
+    
     if (roundCnt === 5) {   
         console.log("GAME OVER");
 
@@ -92,13 +90,81 @@ function winChecker () {
             console.log('There is tie for the best of five!');
         }
 
+        scoreDisplay();
+
         playerWins = 0;
         computerWins = 0;
+        ties = 0;
         roundCnt = 1;
 
     } else {
+        scoreDisplay();
         roundCnt += 1; 
     }
 };
+
+function scoreDisplay () {
+    //player score
+    const score = document.querySelector('.score'); 
+
+    const roundTally = document.createElement('h2');
+    roundTally.classList.add('roundTally');
+    roundTally.textContent = `ROUND ${roundCnt}`;
+
+    const playerTally = document.createElement('div');
+    playerTally.classList.add('playerTally');
+    playerTally.textContent = `Player Score: ${playerWins}`;
+
+    //computer score
+    const computerTally = document.createElement('div');
+    computerTally.classList.add('computerTally');
+    computerTally.textContent = `Computer score: ${computerWins}`;
+
+    const tieTally = document.createElement('div');
+    tieTally.classList.add('tieTally');
+    tieTally.textContent = `Ties: ${ties}`
+
+    //winner
+    score.appendChild(roundTally);
+    score.appendChild(playerTally);
+    score.appendChild(computerTally);
+    score.appendChild(tieTally);
+
+    if (roundCnt === 5) {
+        const totalTally = document.createElement('div');
+        totalTally.classList.add('totalTally');
+        
+        if (playerWins < computerWins){
+            totalTally.textContent = 'Computer wins the best of 5!'
+        } else if (playerWins > computerWins) {
+            totalTally.textContent = 'Player wins the best of 5!'
+        } else {
+            totalTally.textContent = 'There is a tie for the best of 5!';
+        }
+
+        score.appendChild(totalTally);
+        
+    }
+
+
+    
+}
+
+function removeScore () {
+    if (roundCnt > 1) {
+        const score = document.querySelector('.score');
+        const playerTally = document.querySelector('.playerTally');
+        const computerTally = document.querySelector('.computerTally');
+        const tieTally = document.querySelector('.tieTally');
+        const roundTally = document.querySelector('.roundTally');
+
+        score.removeChild(roundTally);
+        score.removeChild(playerTally);
+        score.removeChild(computerTally);
+        score.removeChild(tieTally);
+    }
+
+
+}
 
 game();
