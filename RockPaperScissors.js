2d2e08e148de7ac112832b14e0af874b8438c2fe
Qwarkeh <christine.f.stark@gmail.com>
2022-01-30 15:50:38 -0500

    game starts over when someone wins

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -3,103 +3,64 @@ let computerWins = 0;
 let ties = 0;
 let roundCnt = 1;
 
+game();
+
 function game () {
     
     //Event handlers
     const btnRock = document.querySelector('#btnRock');
     btnRock.addEventListener('click', () => {
         removeScore();
-        console.log(`ROUND ${roundCnt}`);
         let computerSelection = computerPlay();
         playRound(computerSelection, 'Rock');
-        winChecker(roundCnt);
+        scoreDisplay();
     });
 
     const btnPaper = document.querySelector('#btnPaper');
     btnPaper.addEventListener('click', () => {
-        console.log(`ROUND ${roundCnt}`);
+        removeScore();
         let computerSelection = computerPlay();
         playRound(computerSelection, 'Paper');
-        winChecker(roundCnt);
+        scoreDisplay();
     });
 
     const btnScissors = document.querySelector('#btnScissors');
     btnScissors.addEventListener('click', () => {
-        console.log(`ROUND ${roundCnt}`);
+        removeScore();
         let computerSelection = computerPlay();
         playRound(computerSelection, 'Scissors');
-        winChecker(roundCnt);
+        scoreDisplay();
     });
 }
 
-//user RNG to determine computer's choice 
 function computerPlay () {
     scaledNumber = Math.floor((Math.random() * 3) + 1);
 
     if (scaledNumber === 1) {
-        console.log("Computer chose Rock");
         return 'Rock';
     } else if (scaledNumber === 2) {
-        console.log("Computer chose Paper");
         return 'Paper';
     } else if (scaledNumber === 3) {
-        console.log("Computer chose Scissors");
         return 'Scissors';
     }
 };
 
 function playRound (computerSelection, playerSelection) {
     if (playerSelection === computerSelection) {
-        console.log("Tie!")
         ties += 1;
     } else if (playerSelection === 'Rock' && computerSelection === 'Scissors') {
-        console.log("You win, Rock beats Scissors!");
         playerWins += 1;
     } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-        console.log("You lose, Paper beats Rock!");
         computerWins += 1;
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Scissors') {
-        console.log("You lose, Scissors beats Paper");
         computerWins += 1;
     } else if (playerSelection === 'Paper' && computerSelection === 'Rock') {
-        console.log("You win, Paper beats Rock!");
         playerWins += 1;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Paper') {
-        console.log("You win, Scissors beats Paper!");
         playerWins += 1;
     } else if (playerSelection === 'Scissors' && computerSelection === 'Rock') {
-        console.log("You lose, Rock beats Scissors!");
         computerWins += 1;
-    }
-};
-
-function winChecker () {
-    
-    if (roundCnt === 5) {   
-        console.log("GAME OVER");
-
-        console.log(`You won ${playerWins} times.`)
-        console.log(`Computer won ${computerWins} times.`)
-
-        if (playerWins < computerWins){
-            console.log('Computer wins best of five.');
-        } else if (playerWins > computerWins) {
-            console.log('Player wins best of five.');
-        } else {
-            console.log('There is tie for the best of five!');
-        }
-
-        scoreDisplay();
-
-        playerWins = 0;
-        computerWins = 0;
-        ties = 0;
-        roundCnt = 1;
-
-    } else {
-        scoreDisplay();
-        roundCnt += 1; 
     }
 };
 
@@ -124,47 +85,56 @@ function scoreDisplay () {
     tieTally.classList.add('tieTally');
     tieTally.textContent = `Ties: ${ties}`
 
-    //winner
+    //appends
     score.appendChild(roundTally);
     score.appendChild(playerTally);
     score.appendChild(computerTally);
     score.appendChild(tieTally);
 
-    if (roundCnt === 5) {
-        const totalTally = document.createElement('div');
-        totalTally.classList.add('totalTally');
-        
-        if (playerWins < computerWins){
-            totalTally.textContent = 'Computer wins the best of 5!'
-        } else if (playerWins > computerWins) {
-            totalTally.textContent = 'Player wins the best of 5!'
-        } else {
-            totalTally.textContent = 'There is a tie for the best of 5!';
-        }
-
-        score.appendChild(totalTally);
-        
-    }
-
-
+    //winner declaration
+    const totalTally = document.createElement('h3');
+    totalTally.classList.add('totalTally');
     
+    if (playerWins === 5){
+        totalTally.textContent = 'Player wins!';
+        score.appendChild(totalTally);
+
+        playerWins = 0;
+        computerWins = 0;
+        ties = 0;
+        roundCnt = 1;
+
+    } else if (computerWins === 5) {
+        totalTally.textContent = 'Computer wins!';
+        score.appendChild(totalTally);
+
+        playerWins = 0;
+        computerWins = 0;
+        ties = 0;
+        roundCnt = 1;
+
+    } else {
+        roundCnt += 1;
+    }
 }
 
 function removeScore () {
-    if (roundCnt > 1) {
         const score = document.querySelector('.score');
         const playerTally = document.querySelector('.playerTally');
         const computerTally = document.querySelector('.computerTally');
         const tieTally = document.querySelector('.tieTally');
         const roundTally = document.querySelector('.roundTally');
+        const totalTally = document.querySelector('.totalTally');
+
+    if (playerTally !== null) {
 
         score.removeChild(roundTally);
         score.removeChild(playerTally);
         score.removeChild(computerTally);
         score.removeChild(tieTally);
+
+        if (totalTally !== null) {
+            score.removeChild(totalTally);
+        }
     }
-
-
 }
-
-game();
